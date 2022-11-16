0fc14a6564bc15b620cb5902ce9359f5106bce25
nstone <nicholasdavidstone@gmail.com>
2022-11-15 19:13:22 -0800

    Nicks and Loris final adjustments before independant work

--- client/src/components/EditBucketListItem.js
+++ client/src/components/EditBucketListItem.js
@@ -15,85 +15,9 @@ import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 import TextField from '@mui/material/TextField';
 
 
-// const processData = (infoObj) => {
-//     const {name, style, height} = infoObj
-    
-//     let bootStyle= "Boot Style NA"
-//     let bindingStyle= "Binding Style NA"
-//     let boardStyle= "Board Style NA"
-//     let boardHeight = "Board Height NA"
-
-//     if (style === 'All-Mountain') {
-//         if(height === "4"){
-//             boardHeight = "4.6"
-//         }else if (height==="5"){
-//             boardHeight = "5.6"
-//         }else if (height==="6"){
-//             boardHeight = "6.6"
-//         }
-    
-//     }else if(style === 'FreeStyle') {
-//         if(height === "4"){
-//             boardHeight = "3.6"
-//         }else if (height==="5"){
-//             boardHeight = "4.6"
-//         }else if (height==="6"){
-//             boardHeight = "5.6"
-//         }
-//     }else if (style === "Back-Country"){
-//         if(height === "4"){
-//             boardHeight = "3.8"
-//         }else if (height==="5"){
-//             boardHeight = "4.8"
-//         }else if (height==="6"){
-//             boardHeight = "5.8"
-//         }
-//     }
-
-//     if (style === 'All-Mountain') {
-//         bootStyle = "Stiff"
-//     }else if(style === 'FreeStyle') {
-//         bootStyle = 'Flexible'
-//     }else if (style === "Back-Country"){
-//         bootStyle = "Mid-Range Flex"
-//     }
-
-//     if (style === 'All-Mountain') {
-//         bindingStyle = "Stiff"
-//     }else if(style === 'FreeStyle') {
-//         bindingStyle = 'Flexible'
-//     }else if (style === "Back-Country"){
-//         bindingStyle = "Mid-Range Flex"
-//     }
-
-//     if (style === 'All-Mountain') {
-//         boardStyle = "Directional Twin"
-//     }else if(style === 'FreeStyle') {
-//         boardStyle = 'True Twin'
-//     }else if (style === "Back-Country"){
-//         boardStyle = "Directional"
-//     }
-
-//     // 'All-Mountain','FreeStyle', 'Back-Country'
-
-//     return {
-//         name,
-//         style,
-//         height,
-//         boardStyle,
-//         bindingStyle,
-//         bootStyle,
-//         boardHeight,
-
-//     }
-// }
-
-
 const EditBucketListItem = () => {
 
-    // const [name,setName] = useState('')
     const [name, setName] = useState('')
-    // const [footSize, setFootSize] =useState('')
     const [activity, setActivity] = useState('')
     const [date, setDate] = useState('')
     const [description, setDescription] = useState('')
@@ -104,36 +28,20 @@ const EditBucketListItem = () => {
     const navigate = useNavigate()
 
 
-    // might be only for have a list selector not general input box
-    // const handleChangeName = (event) => {
-    //     setName(event.target.value);
-    //   };
-    // const handleChangeDate = (event) => {
-    //     setDate(event.target.value);
-    // };
-    // const handleChangeactivity = (event) => {
-    //     setActivity(event.target.value);
-    //     };
-    // const handleChangeDescription = (event) => {
-    // setDescription(event.target.value);
-    // };
-
 
     useEffect(()=>{
         axios.get(`http://localhost:8000/api/oneItem/${id}`,{withCredentials:true})
         .then((res)=>{
-            // setName(res.data.name)
             setName(res.data.name)
             setDate(res.data.date)
             setActivity(res.data.activity)
             setDescription(res.data.description)
-            // setFootSize(res.data.footSize)
+
 
 
         })
         .catch((err)=>{
             console.log(err)
-            // setNoId('Pets not found using that ID')
         })
     },[])
 
@@ -145,7 +53,7 @@ const submitHandler = (e) =>{
         description,
         activity,
     }
-        // processData({style, height})
+
         ,{withCredentials:true, credentials:'include'})
     .then((res)=>{
         console.log(res)
@@ -181,52 +89,17 @@ return (
                 noValidate
                 autoComplete="off"
                 >
-                < TextField id="outlined-basic" size="small" label="Name" variant="outlined" onChange={(e)=> setName(e.target.value)}/>
+                < TextField id="outlined-basic" value= {name} size="small" label="Name" variant="outlined" onChange={(e)=> setName(e.target.value)}/>
             </Box>
 
 
                 
                 
-                {/* <Box sx={{ minWidth: 120 }}>
-                    <FormControl size="small" fullWidth>
-                        <InputLabel id="demo-simple-select-label">
-                            Style
-                        </InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={name}
-                        label="Name"
-                        onChange={handleChangeName}
-                        >
-                        <MenuItem value="All-Mountain">All-Mountain</MenuItem>
-                        <MenuItem value="FreeStyle">FreeStyle</MenuItem>
-                        <MenuItem value="Back-Country">Back-Country</MenuItem>
-                    </Select>
-                </FormControl>
-            </Box> */}
+               
             { errors.name ? <span className="text-danger">{errors.name.message}</span> :null}<br></br>
     </div>
 
     <div className="w-50 mx-auto" style={{height:"50px"}}>
-        {/* <Box sx={{ minWidth: 120 }}>
-                <FormControl size="small" fullWidth>
-                    <InputLabel id="demo-simple-select-label">
-                        Height
-                    </InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={height}
-                    label="height"
-                    onChange={handleChangeHeight}
-                    >
-                    <MenuItem value="4">4</MenuItem>
-                    <MenuItem value="5">5</MenuItem>
-                    <MenuItem value="6">6</MenuItem>
-                </Select>
-            </FormControl>
-        </Box> */}
 
 <Box
                 sx={{
@@ -235,32 +108,14 @@ return (
                 noValidate
                 autoComplete="off"
                 >
-                < TextField id="outlined-basic" size="small" label="activity" variant="outlined" onChange={(e)=> setActivity(e.target.value)}/>
+                < TextField id="outlined-basic" value= {activity} size="small" label="activity" variant="outlined" onChange={(e)=> setActivity(e.target.value)}/>
             </Box>
 
         { errors.activity ? <span className="text-danger">{errors.activity.message}</span> :null}<br></br>
     </div>
 
     <div className="w-50 mx-auto" style={{height:"50px"}}>
-        {/* <Box sx={{ minWidth: 120 }}>
-                <FormControl size="small" fullWidth>
-                    <InputLabel id="demo-simple-select-label">
-                        Height
-                    </InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={height}
-                    label="height"
-                    onChange={handleChangeHeight}
-                    >
-                    <MenuItem value="4">4</MenuItem>
-                    <MenuItem value="5">5</MenuItem>
-                    <MenuItem value="6">6</MenuItem>
-                </Select>
-            </FormControl>
-        </Box> */}
-
+       
             <Box
                 sx={{
                     '& > :not(style)': { mb: 0, width: '30ch', },
@@ -268,31 +123,14 @@ return (
                 noValidate
                 autoComplete="off"
                 >
-                < TextField id="outlined-basic" size="small" label="Description" variant="outlined" onChange={(e)=> setDescription(e.target.value)}/>
+                < TextField id="outlined-basic" value= {description} size="small" label="Description" variant="outlined" onChange={(e)=> setDescription(e.target.value)}/>
             </Box>
 
         { errors.description ? <span className="text-danger">{errors.description.message}</span> :null}<br></br>
     </div>
 
     <div className="w-50 mx-auto" style={{height:"50px"}}>
-        {/* <Box sx={{ minWidth: 120 }}>
-                <FormControl size="small" fullWidth>
-                    <InputLabel id="demo-simple-select-label">
-                        Height
-                    </InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={height}
-                    label="height"
-                    onChange={handleChangeHeight}
-                    >
-                    <MenuItem value="4">4</MenuItem>
-                    <MenuItem value="5">5</MenuItem>
-                    <MenuItem value="6">6</MenuItem>
-                </Select>
-            </FormControl>
-        </Box> */}
+        
             <Box
                 sx={{
                     '& > :not(style)': { mb: 0, width: '30ch', },
@@ -300,7 +138,7 @@ return (
                 noValidate
                 autoComplete="off"
                 >
-                < TextField id="outlined-basic" size="small" label="Date" variant="outlined" onChange={(e)=> setDate(e.target.value)}/>
+                < TextField id="outlined-basic" value= {date}  size="small" label="Date" variant="outlined" onChange={(e)=> setDate(e.target.value)}/>
             </Box>
         { errors.date ? <span className="text-danger">{errors.date.message}</span> :null}<br></br>
     </div>
